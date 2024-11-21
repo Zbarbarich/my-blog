70fba4c46f789e7a04725be6aef3c9bc91115285
Zach Barbarich <zachery.barbarich@gmail.com>
2024-11-21 12:02:09 -0500

    feat: Add BlogPost and BlogList components

--- src/data/posts.js
+++ src/data/posts.js
@@ -25,10 +25,10 @@ export const posts = [
     }, 
     {
       id: 4,
-      title: "The Future of AI",
-      content: "AI is changing the world faster than we can imagine...",
-      author: "Jane Doe",
-      date: "2024-03-15",
-      readTime: 5
-        }
+      title: "Cloud Computing advantages and disadvantages",
+      content: "Cloud computing is a powerful tool that can be used to store and access data from anywhere in the world. It is a great way to save time and money, but it also has some disadvantages. In this blog post, we will discuss the advantages and disadvantages of cloud computing.",
+      author: "John Smith",
+      date: "2024-03-18",
+      readTime: 10
+    }
   ];
