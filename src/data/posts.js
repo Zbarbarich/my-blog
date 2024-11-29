d0842d55f0d8d27afe4d3d33df235e506a64163f
Zach Barbarich <zachery.barbarich@gmail.com>
2024-11-28 22:53:38 -0500

    feat: add read more toggle and updated blogpost to include like button and comment section

--- src/data/posts.js
+++ src/data/posts.js
@@ -2,7 +2,7 @@ export const posts = [
     {
       id: 1,
       title: "Getting Started with React",
-      content: "React is a powerful library for building user interfaces...",
+      content: "React is a powerful library for building user interfaces. It is a great way to save time and money, but it also has some disadvantages. In this blog post, we will discuss the advantages and disadvantages of React. React is a powerful library for building user interfaces. It is a great way to save time and money, but it also has some disadvantages. In this blog post, we will discuss the advantages and disadvantages of React.",
       author: "Jane Doe",
       date: "2024-03-15",
       readTime: 5
@@ -10,7 +10,7 @@ export const posts = [
     {
       id: 2,
       title: "Understanding Props",
-      content: "Props are the way we pass data between React components...",
+      content: "Props are the way we pass data between React components. They are a powerful tool that can be used to pass data between components. They are a great way to save time and money, but they also have some disadvantages. In this blog post, we will discuss the advantages and disadvantages of props. Props are the way we pass data between React components. They are a powerful tool that can be used to pass data between components. They are a great way to save time and money, but they also have some disadvantages. In this blog post, we will discuss the advantages and disadvantages of props.",
       author: "John Smith",
       date: "2024-03-16",
       readTime: 3
@@ -18,7 +18,7 @@ export const posts = [
     {
       id: 3,
       title: "The Future of AI",
-      content: "AI is changing the world faster than we can imagine...",
+      content: "AI is changing the world faster than we can imagine. It is a powerful tool that can be used to solve problems and make decisions. It is a great way to save time and money, but it also has some disadvantages. In this blog post, we will discuss the advantages and disadvantages of AI. AI is changing the world faster than we can imagine. It is a powerful tool that can be used to solve problems and make decisions. It is a great way to save time and money, but it also has some disadvantages. In this blog post, we will discuss the advantages and disadvantages of AI.",
       author: "Jane Doe",
       date: "2024-03-15",
       readTime: 5
@@ -26,7 +26,7 @@ export const posts = [
     {
       id: 4,
       title: "Cloud Computing advantages and disadvantages",
-      content: "Cloud computing is a powerful tool that can be used to store and access data from anywhere in the world. It is a great way to save time and money, but it also has some disadvantages. In this blog post, we will discuss the advantages and disadvantages of cloud computing.",
+      content: "Cloud computing is a powerful tool that can be used to store and access data from anywhere in the world. It is a great way to save time and money, but it also has some disadvantages. In this blog post, we will discuss the advantages and disadvantages of cloud computing. Cloud computing is a powerful tool that can be used to store and access data from anywhere in the world. It is a great way to save time and money, but it also has some disadvantages. In this blog post, we will discuss the advantages and disadvantages of cloud computing.",
       author: "John Smith",
       date: "2024-03-18",
       readTime: 10
